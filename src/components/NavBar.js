d585a482714c0e87af5044101bb9a759a4349a8f
jrodriguezc160 <jrcortes02102005@educarex.es>
2023-11-28 10:40:29 +0100

    logogogogoogog

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,7 +16,7 @@ export default function NavBar(props) {
             {window.innerWidth < 862 ? (
               <a>
                 <img
-                  src="../assets/logo512.png"
+                  src="assets/logo512.png"
                   alt="react-logo"
                   style={{ width: '16px', height: '16px' }}
                 />
